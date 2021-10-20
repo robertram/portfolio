e46f8dd69c6f1c4ad2467837d3d256245fdf4339
Robert Ramirez <rsft6000@gmail.com>
2021-10-20 14:44:11 -0600

    post page local images included

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,13 +1,25 @@
 import * as React from "react";
 import Layout from "../../components/layout";
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { graphql } from "gatsby";
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle="Super Cool Blog Posts">
-      <p>My blog post contents will go here (eventually).</p>
+      <h1>{data.mdx.title}</h1>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage
+        image={image}
+        alt={data.mdx.frontmatter.hero_image_alt}
+      />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -16,15 +28,23 @@ const BlogPost = ({ data }) => {
 };
 
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: { eq: $id }) {
+  query($id: String) {
+    mdx(id: {eq: $id}) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
-`;
+`
 
 export default BlogPost;
