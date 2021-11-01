a859da78b3ec6d2a691932d53120b7194d50094a
Robert Ramirez <rsft6000@gmail.com>
2021-11-01 01:53:10 -0600

    strapi-integration Header and Layout added with camelcase

---
+++ src/components/Layout.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useContext } from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import Header from "./Header";
+import { GlobalStyle } from "../styles/theme";
+import Theme, { darkTheme, lightTheme } from "../styles/theme";
+import { ThemeContext, saveThemeModePrefences } from "../context/themeContext";
+import Seo from "./Seo";
+
+export interface Props {
+  pageTitle?: string;
+  children;
+  seo?: any;
+}
+
+const Layout = (props: Props) => {
+  const { theme } = useContext(ThemeContext);
+  const { pageTitle, children } = props;
+  useEffect(() => {
+    saveThemeModePrefences();
+  }, []);
+
+  const data = useStaticQuery(graphql`
+    query {
+      site(siteMetadata: { title: {}, siteUrl: {} }) {
+        siteMetadata {
+          title
+          siteUrl
+        }
+      }
+      strapiHomepage {
+        seo {
+          metaTitle
+          metaDescription
+          shareImage {
+            localFile {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <Theme theme={theme === "dark" ? darkTheme : lightTheme}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>
+          {`${pageTitle && pageTitle}`} | {data.site.siteMetadata.title}
+        </title>
+        <meta id="colorScheme" name="color-scheme" content={theme || "light"} />
+      </Helmet>
+      <Seo seo={data.strapiHomepage.seo} />
+      <LayoutContainer>
+        <GlobalStyle theme={theme === "dark" ? darkTheme : lightTheme} />
+        <Header />
+        <main>{children}</main>
+      </LayoutContainer>
+    </Theme>
+  );
+};
+
+const LayoutContainer = styled.div`
+  transition: background 0.2s ease-out;
+  background-color: ${(props) => props.theme.global.bg};
+`;
+
+export default Layout;
