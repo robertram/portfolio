a2c99dab83dcd69e996e3106e850b373f9c6833c
Robert Ramirez <rsft6000@gmail.com>
2021-11-07 09:40:42 -0600

    header gatsby-ssr and gatsby-browser global styles

---
+++ gatsby-browser.js
@@ -0,0 +1,14 @@
+// gatsby-browser.js
+const React = require("react");
+//require("./src/scss/index.scss");
+const GlobalStyles = require("./src/styles/theme").default;
+const Layout = require("./src/components/Layout").default;
+
+exports.wrapPageElement = ({ element }) => {
+  return (
+    <>
+      <GlobalStyles />
+      <Layout>{element}</Layout>
+    </>
+  );
+};
