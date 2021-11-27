bd831d0513f5eeb09edfb4f2f4e0958672577eb3
Robert Ramirez <rsft6000@gmail.com>
2021-11-27 02:22:55 -0600

    hero shared card added and used in Education section

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,78 +1,11 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Button from "./Button";
+import { SharedCard as EducationCard } from "./SharedCard";
 
 interface Props {
   educationData: any;
 }
 
-interface CardProps {
-  title: string;
-  description?: string;
-  picture?: any;
-  date?: string;
-  link?: string;
-  provider?: string;
-}
-
-const EducationCard = (props: CardProps) => {
-  const { title, description, picture, date, link, provider } = props;
-  return (
-    <EducationCardContainer className="border-2 rounded-xl mb-4 bg-background2-light dark:bg-background2-dark p-6">
-      <div className="flex flex-col">
-        {picture && (
-          <div className="EducationCard__imageContainer sm:mr-6 mb-4">
-            <img
-              src={picture.img[0].url}
-              alt={title}
-              className="EducationCard__image w-full object-cover object-center"
-            />
-          </div>
-        )}
-        <div className="w-full flex flex-col justify-center sm:mb-6 sm:m-0">
-          <h1 className="dark:text-text-dark text-text-light text-4xl sm:text-5xl mb-2 sm:mb-2">
-            {title}
-          </h1>
-          {description && (
-            <h2 className="dark:text-text-dark text-text-light text-2xl sm:text-xl">
-              {description}
-            </h2>
-          )}
-          {provider && (
-            <h2 className="dark:text-text-dark text-text-light text-2xl sm:text-xl">
-              {provider}
-            </h2>
-          )}
-          {date && (
-            <p className="dark:text-text-dark text-text-light text-1xl">
-              {date}
-            </p>
-          )}
-          {link && <Button text="See project" blank link={link} />}
-        </div>
-      </div>
-    </EducationCardContainer>
-  );
-};
-const EducationCardContainer = styled.div`
-  @media (min-width: 640px) {
-    min-height: 250px;
-  }
-  .EducationCard {
-    &__imageContainer {
-      width: 100%;
-      @media (min-width: 640px) {
-        max-height: 200px;
-      }
-    }
-    &__image {
-      width: 100%;
-      @media (min-width: 640px) {
-        max-height: 200px;
-      }
-    }
-  }
-`;
 const Education = (props: Props) => {
   const { educationData } = props;
   const [educationArray, setEducationArray] = useState([]);
@@ -93,7 +26,7 @@ const Education = (props: Props) => {
     <EducationContainer id="education">
       <div className="EducationCard__parent m-auto py-8 flex justify-center items-center">
         <div className="w-full">
-          <h2 className="text-5xl mb-6">My Education</h2>
+          <h3 className="text-5xl mb-6">My Education</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 ">
             {educationArray &&
               educationArray.map((item: any, index: number) => {
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,91 +1,10 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Button from "./Button";
+import { SharedCard as WorkCard } from "./SharedCard";
 
 interface Props {
   workData: any;
 }
-
-interface CardProps {
-  title: string;
-  description?: string;
-  picture?: any;
-  date?: string;
-  link?: string;
-  company?: string;
-  teches?: any;
-}
-
-const WorkCard = (props: CardProps) => {
-  const { title, description, picture, date, link, company, teches } = props;
-  return (
-    <WorkCardContainer className="h-full mb-4 bg-background2-light dark:bg-background2-dark rounded-xl shadow-md hover:shadow-2xl transition-all duration-500">
-      <div className="flex flex-col">
-        {picture && (
-          <div className="WorkCard__imageContainer sm:mr-6 mb-4 rounded-xl rounded-tl-xl rounded-tr-xl">
-            <img
-              src={picture.img[0].url}
-              alt={title}
-              className="WorkCard__image w-full object-cover object-center rounded-tl-xl rounded-tr-xl "
-            />
-          </div>
-        )}
-        <div className="w-full flex flex-col justify-center sm:m-0 border-2 p-6 pt-0">
-          <h4 className="dark:text-text-dark text-text-light text-4xl sm:text-4xl mb-2">
-            {title}
-          </h4>
-          {company && (
-            <p className="dark:text-text-dark text-text-light text-3xl sm:text-2xl font-semibold mb-2 ">
-              {company}
-            </p>
-          )}
-          {description && (
-            <p className="dark:text-text-dark text-text-light text-xl sm:text-base mb-2 ">
-              {description}
-            </p>
-          )}
-          {date && (
-            <p className="dark:text-text-dark text-text-light text-md font-semibold">
-              {date}
-            </p>
-          )}
-          <div className="flex mt-2 flex-wrap">
-            {teches &&
-              teches.map((item: any, index: number) => (
-                <p
-                  key={index}
-                  className="text-sm dark:text-gray-300 bg-background-light dark:bg-background-dark w-min h-full px-2 py-1 mr-2 mb-2 rounded-lg whitespace-nowrap"
-                >
-                  {item.name}
-                </p>
-              ))}
-          </div>
-
-          {link && <Button text="See project" blank link={link} />}
-        </div>
-      </div>
-    </WorkCardContainer>
-  );
-};
-const WorkCardContainer = styled.div`
-  @media (min-width: 640px) {
-    min-height: 250px;
-  }
-  .WorkCard {
-    &__imageContainer {
-      width: 100%;
-      @media (min-width: 640px) {
-        max-height: 200px;
-      }
-    }
-    &__image {
-      width: 100%;
-      @media (min-width: 640px) {
-        max-height: 200px;
-      }
-    }
-  }
-`;
 
 const Work = (props: Props) => {
   const { workData } = props;
@@ -108,7 +27,7 @@ const Work = (props: Props) => {
     <WorkContainer id="work">
       <div className="WorkCard__parent m-auto py-8 flex justify-center items-center">
         <div className="w-full">
-          <h3 className="text-5xl font-medium mb-6">My Work</h3>
+          <h3 className="text-5xl mb-6">My Work</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
             {workArray &&
               workArray.map((item: any, index: number) => {
